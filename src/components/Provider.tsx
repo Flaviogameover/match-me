82731e056fb61bf48761ed990588df8d09f2e6b4
Flaviogameover <flaviogameover@gmail.com>
2024-11-12 17:18:00 -0300

    First build

---
+++ src/components/Provider.tsx
@@ -0,0 +1,16 @@
+import {NextUIProvider} from "@nextui-org/react";
+import {ReactNode} from "react";
+
+type Props = {
+	children: ReactNode;
+}
+export const Providers = ({children}: Props) => {
+
+
+	return (
+		<NextUIProvider>
+			{children}
+		</NextUIProvider>
+	)
+
+}
